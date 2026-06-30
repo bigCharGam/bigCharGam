5350e18f366a49811dbc058048c53a98748ce31c
eionww <eionhwan@gmail.com>
2026-06-30 10:46:09 +0900

    Attack

--- Assets/Scripts/Test1/EnemyBase.cs
+++ Assets/Scripts/Test1/EnemyBase.cs
@@ -4,4 +4,8 @@ public class EnemyBase : CharacterBase
 {
     [Header("EnemyStats")]
     protected float test1;
+
+    [SerializeField] protected float detectionRange;
+    [SerializeField] protected LayerMask playerLayer;
+    [SerializeField] protected Transform playerTransform;
 }
--- Assets/Scripts/Test1/TestEnemy1.cs
+++ Assets/Scripts/Test1/TestEnemy1.cs
@@ -1,7 +1,13 @@
 using UnityEngine;
+using System.Collections;
 
 public class TestEnemy1 : EnemyBase
 {
+    [Header("Skill 1 Slash")]
+    [SerializeField] private GameObject skill1Hitbox;
+    [SerializeField] private float skill1Damage = 10f;
+
+    private Animator anim;
     private void Reset()
     {
         maxHealth = 150f;
@@ -12,5 +18,29 @@ public class TestEnemy1 : EnemyBase
     protected override void Start()
     {
         base.Start();
+        anim = GetComponent<Animator>();
+
+        StartCoroutine(SkillTest());
+    }
+    public void UseSkill1_Slash()
+    {
+        anim.SetTrigger("Skill1_Slash");
+    }
+    private void HitboxEnable_Skill1()
+    {
+        skill1Hitbox.SetActive(true);
+        skill1Hitbox.GetComponent<EnemyAttackHitbox>().damage = skill1Damage;
+    }
+    private void HitboxDisable_Skill1()
+    {
+        skill1Hitbox.SetActive(false);
+    }
+    private IEnumerator SkillTest()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(3f);
+            UseSkill1_Slash();
+        }
     }
 }
