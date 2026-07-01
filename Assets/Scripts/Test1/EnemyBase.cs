9038ea6ee6bb869f2454021aaec7dab5074ec775
eionww <eionhwan@gmail.com>
2026-07-01 12:11:25 +0900

    dd

--- Assets/Scripts/Test1/EnemyBase.cs
+++ Assets/Scripts/Test1/EnemyBase.cs
@@ -1,11 +1,126 @@
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
+
+public enum EnemyState
+{
+    Idle,
+    Patrol,
+    Battle,
+}
+
+[System.Serializable] //인스펙터에서 잘 보이게?
+public struct Waypoint
+{
+    public Transform transform;
+    public float waitTime;
+}
 
 public class EnemyBase : CharacterBase
 {
-    [Header("EnemyStats")]
-    protected float test1;
-
+    [Header("EnemyBase")]
     [SerializeField] protected float detectionRange;
     [SerializeField] protected LayerMask playerLayer;
+    [SerializeField] protected Waypoint[] waypoints;
+    [SerializeField] protected bool patrolLoop = true;
+
+    protected EnemyState currentState = EnemyState.Patrol;
+    protected Animator anim;
     [SerializeField] protected Transform playerTransform;
+    protected Rigidbody2D rb;
+    protected float distanceToPlayer;
+
+    private int currentWaypointIndex = 0;
+    private bool isWating = false;
+
+    protected void MoveToTarget(Transform target, float moveSpeed)
+    {
+        if (target == null) return;
+        Vector2 direction = (target.position - transform.position).normalized;
+        rb.linearVelocity = new Vector2(direction.x * moveSpeed, rb.linearVelocity.y);
+        transform.localScale = new Vector3(direction.x > 0 ? 1 : -1, 1, 1);
+    }
+    protected virtual void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    protected virtual void Update()
+    {
+        if (playerTransform != null)
+            distanceToPlayer = Vector2.Distance(transform.position, playerTransform.position);
+
+        switch (currentState)
+        {
+            case EnemyState.Idle:
+                HandleIdle();
+                break;
+            case EnemyState.Patrol:
+                HandlePatrol();
+                break;
+            case EnemyState.Battle:
+                HandleBattle();
+                break;
+        }
+    }
+
+    private bool PlayerDetect()
+    {
+        Collider2D player = Physics2D.OverlapCircle(transform.position, detectionRange, playerLayer);
+        if (player != null)
+        {
+            currentState = EnemyState.Battle;
+            anim.SetBool("isMoving", true);
+        }
+        return player != null;
+    }
+
+    protected virtual void HandleIdle()
+    {
+        if (PlayerDetect()) return;
+    }
+
+    private void HandlePatrol()
+    {
+        if (waypoints.Length == 0)
+        {
+            currentState = EnemyState.Idle;
+            return;
+        }
+        if (isWating) return;
+
+        if (PlayerDetect()) return;
+
+        Transform waypoint = waypoints[currentWaypointIndex].transform;
+        MoveToTarget(waypoint, MoveSpeed);
+
+        if (Vector2.Distance(transform.position, waypoint.position) < 0.1f)
+        {
+            isWating = true;
+            StartCoroutine(WaitAtWaypoint(waypoints[currentWaypointIndex].waitTime));
+
+            currentWaypointIndex++;
+            if (currentWaypointIndex >= waypoints.Length)
+            {
+                if (patrolLoop)
+                {
+                    currentWaypointIndex = 0;
+                }
+                else
+                {
+                    currentState = EnemyState.Idle;
+                }
+            }
+        }
+    }
+    IEnumerator WaitAtWaypoint(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        isWating = false;
+    }
+
+    protected virtual void HandleBattle()
+    {
+
+    }
 }
--- Assets/Scripts/Test1/TestEnemy1.cs
+++ Assets/Scripts/Test1/TestEnemy1.cs
@@ -1,46 +1,94 @@
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 
 public class TestEnemy1 : EnemyBase
 {
     [Header("Skill 1 Slash")]
     [SerializeField] private GameObject skill1Hitbox;
-    [SerializeField] private float skill1Damage = 10f;
+    [SerializeField] private float skill1Damage;
+    [SerializeField] private float skill1MinUseRange;
 
-    private Animator anim;
+    [Header("Battle")]
+    [SerializeField] private float runSpeed;
+
+    private bool isAttacking = false;
+    private int selectedSkillIndex = -1;
+
     private void Reset()
     {
         maxHealth = 150f;
         MoveSpeed = 8f;
-        baseAttackPower = 12f;
+        skill1Damage = 10f;
+        skill1MinUseRange = 2f;
+        runSpeed = 12f;
     }
 
     protected override void Start()
     {
         base.Start();
         anim = GetComponent<Animator>();
+    }
 
-        StartCoroutine(SkillTest());
-    }
-    public void UseSkill1_Slash()
+    protected override void Update()
     {
-        anim.SetTrigger("Skill1_Slash");
+        base.Update();
     }
+
+    protected override void HandleBattle()
+    {
+        if (isAttacking) return;
+
+        if (selectedSkillIndex == -1)
+            selectedSkillIndex = Random.Range(0, 1);
+
+        float requiredRange = GetMinSkillRange(selectedSkillIndex);
+
+        if (distanceToPlayer > requiredRange)
+        {
+            MoveToTarget(playerTransform, runSpeed);
+        }
+        else
+        {
+            rb.linearVelocity = new Vector2(0f, rb.linearVelocity.y);
+            isAttacking = true;
+            UseSkill(selectedSkillIndex);
+            selectedSkillIndex = -1;
+        }
+    }
+
+    private float GetMinSkillRange(int index)
+    {
+        switch (index)
+        {
+            case 0: return skill1MinUseRange;
+            default: return 2f;
+        }
+    }
+
+    private void UseSkill(int index)
+    {
+        switch (index)
+        {
+            case 0: anim.SetTrigger("Skill1_Slash"); break;
+        }
+    }
+
+    // Animation Event에서 호출
+    private void OnAttackEnd()
+    {
+        isAttacking = false;
+    }
+
+    // Animation Event에서 호출
     private void HitboxEnable_Skill1()
     {
         skill1Hitbox.SetActive(true);
         skill1Hitbox.GetComponent<EnemyAttackHitbox>().damage = skill1Damage;
     }
+
+    // Animation Event에서 호출
     private void HitboxDisable_Skill1()
     {
         skill1Hitbox.SetActive(false);
-    }
-    private IEnumerator SkillTest()
-    {
-        while (true)
-        {
-            yield return new WaitForSeconds(3f);
-            UseSkill1_Slash();
-        }
     }
 }
