1dc036d44d261a16553b29d85d950e29b970a291
eionww <eionhwan@gmail.com>
2026-08-05 11:38:07 +0900

    Expbar

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using System.Collections;
 
 public class UIManager : MonoBehaviour
 {
@@ -8,11 +10,16 @@ public class UIManager : MonoBehaviour
     [SerializeField] private Image playerHPInnerbar;
     [SerializeField] private Image playerMPInnerbar;
     [SerializeField] private Image bossHPInnerbar;
+    [SerializeField] private Image expBar;
+    [SerializeField] private TextMeshProUGUI expText;
     [SerializeField] private float lerpSpeed = 8f;
+    [SerializeField] private float lerpSpeedExp = 3f;
+
 
     private float playerHPTarget = 1f;
     private float playerMPTarget = 1f;
     private float bossHPTarget = 1f;
+    private float expTarget = 0f;
 
     [Header("Skill")]
     [SerializeField] private Image[] skillOnImage;
@@ -32,14 +39,11 @@ public class UIManager : MonoBehaviour
 
     void Update()
     {
-        if (playerHPInnerbar != null)
             playerHPInnerbar.fillAmount = Mathf.Lerp(playerHPInnerbar.fillAmount, playerHPTarget, lerpSpeed * Time.deltaTime);
-
-        if (playerMPInnerbar != null)
             playerMPInnerbar.fillAmount = Mathf.Lerp(playerMPInnerbar.fillAmount, playerMPTarget, lerpSpeed * Time.deltaTime);
-
-        if (bossHPInnerbar != null)
             bossHPInnerbar.fillAmount = Mathf.Lerp(bossHPInnerbar.fillAmount, bossHPTarget, lerpSpeed * Time.deltaTime);
+            expBar.fillAmount = Mathf.MoveTowards(expBar.fillAmount, expTarget, lerpSpeedExp * Time.deltaTime);
+            expText.text = BattleManager.instance.expLevel.ToString();
     }
 
     public void UpdatePlayerHP(float currentHp, float maxHp)
@@ -57,8 +61,29 @@ public class UIManager : MonoBehaviour
     public void UpdateBossHP(float currentHp, float maxHp)
     {
         if (bossHPInnerbar == null) return;
-        Debug.Log($"Updating Boss HP: Current HP = {currentHp}, Max HP = {maxHp}");
         bossHPTarget = NormalizeRatio(currentHp, maxHp);
+    }
+    public void UpdateExp(float currentExp, float maxExp)
+    {
+        if (expBar == null) return;
+        expTarget = NormalizeRatio(currentExp, maxExp);
+    }
+
+    public void ResetExpBar()
+    {
+        if (expBar == null) return;
+        expBar.fillAmount = 0f;
+        expTarget = 0f;
+    }
+
+    public IEnumerator WaitForExpBar()
+    {
+        if (expBar == null) yield break;
+
+        while (Mathf.Abs(expBar.fillAmount - expTarget) > 0.01f)
+            yield return null;
+
+        expBar.fillAmount = expTarget;
     }
 
     private float NormalizeRatio(float current, float max)
